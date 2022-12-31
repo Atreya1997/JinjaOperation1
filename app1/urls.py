3c6db5cdeb8e3e22861fc762ceeb8a06518c503f
Your Name <you@example.com>
2022-12-31 12:45:32 +0530

    first commit

---
+++ app1/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from app1.views import *
+app_name='Atreya'
+
+urlpatterns = [
+    path('Atreya1/',Atreya1,name='Atreya1'),
+]
---
+++ app2/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Atreya2(request):
+    return render(request,'Atreya2.html')
